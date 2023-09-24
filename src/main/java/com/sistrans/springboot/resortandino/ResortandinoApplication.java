55b0dc32b33d204c57313c08ba751afde48f0321
Samuel Santa <s.santaa@uniandes.edu.co>
2023-09-24 15:55:14 -0500

    Proyecto Java Spring

---
+++ src/main/java/com/sistrans/springboot/resortandino/ResortandinoApplication.java
@@ -0,0 +1,13 @@
+package com.sistrans.springboot.resortandino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResortandinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResortandinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistrans/springboot/resortandino/ResortandinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistrans.springboot.resortandino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResortandinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
